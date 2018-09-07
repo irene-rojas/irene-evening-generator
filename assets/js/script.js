4b1b4acaf3751a45d026319d164750da81bb6a5f
Irene R <irene@smallwondermedia.com>
2018-09-06 21:05:59 -0400

    6

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,10 +28,11 @@ $(document).ready(function() {
             var recipeLink = $("<a />", {
                 name : "link",
                 href : "https://www.themealdb.com/meal.php?c=" + recipeId,
-                text : "Recipe"
+                text : "Recipe from The MealDB"
             });
             // console.log(recipeLink);
 
+            // making the image a link
             resultDiv.prepend(image);
             $(image).wrap("<a href=" + "https://www.themealdb.com/meal.php?c=" + recipeId +" target='_blank'></a>");
             resultDiv.append(recipeLink);
@@ -40,7 +41,7 @@ $(document).ready(function() {
         });
 
         
-        // WORKING MOVIE CODE
+        // STATIC MOVIE CODE
         // var movieArray = ["Jurassic Park", "Inception", "The Muppets", "Inside Out", "Gravity", "Slumdog Millionaire", "The Notebook"];
         // var randomMovie = movieArray[Math.floor(Math.random()*movieArray.length)];
 
@@ -61,49 +62,59 @@ $(document).ready(function() {
         // });
 
 
-
-        // EXPERIMENTAL MOVIE CODE
+        // DYNAMIC MOVIE CODE
         var selectedCuisine = [$("#dropDown option:selected").text()];
         var queryURL = "https://www.omdbapi.com/?apikey=d9666985&s=" + selectedCuisine;
-        console.log(queryURL);
+        // console.log(queryURL);
 
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
+            // console.log(response);
            $("#movieResult").empty();
 
            var result = response.Search.slice(0, 5);
-           console.log(result);
+        //    console.log(result);
 
            var movieDiv = $("<div class='movie'>");
 
             var movieSuggestion = result[Math.floor(Math.random()*result.length)];
-            console.log(movieSuggestion);
+            // console.log(movieSuggestion);
             movieDiv.append(movieSuggestion);
 
             var imgURL = movieSuggestion.Poster;
             var image = $("<img>").attr("src", imgURL);
-            movieDiv.prepend(image);
+            // movieDiv.prepend(image);
 
-            var title = movieSuggestion.Title;
-            var titleText = $("<p>").text(title);
+            var plotSource = movieSuggestion.imdbID;
+            var plotURL = "https://www.imdb.com/title/" + plotSource + "/plotsummary?ref_=tt_ql_stry_2";
+            var plotLink = $("<a />", {
+                name : "link",
+                href : plotURL,
+                // text : "Plot from IMDB",
+                target : "_blank"
+            });
+            // console.log(plotSource);
+            // console.log(plotURL);
+            movieDiv.prepend(image);
+            // making the image a link
+            $(image).wrap("<a href=" + plotURL + " target='_blank'></a>");
+            movieDiv.append(plotLink);
+
+            var movieTitle = movieSuggestion.Title;
+            var titleText = $("<p>").text(movieTitle);
             movieDiv.append(titleText);
 
-            var plot = movieSuggestion.Plot;
-            var plotText = $("<p>").text(plot);
-            movieDiv.append(plotText);
+            var year = movieSuggestion.Year;
+            var yearText = $("<p>").text(year);
+            movieDiv.append(yearText);
 
            $("#movieResult").append(movieDiv);
         });
 
-
-
     });
-
-
 
 });
 
